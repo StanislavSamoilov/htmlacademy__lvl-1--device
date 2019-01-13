c9e222c01b2ff7121e6268e43c8c342af1130829
SSO <40271618+StanislavSamoilov@users.noreply.github.com>
2019-01-13 14:15:20 +0200

    add slider create function

---
+++ src/js/components/createSlider.js
@@ -0,0 +1,27 @@
+const createSlider = ({ sliderControls="slider__controls",
+                        sliderControl="slider__control",
+                        sliderControlActive=`${sliderControl}--active`,
+                        sliderItem="slider__item",
+                        sliderItemActive=`${sliderItem}--active`,
+                      } = {}) => 
+  {
+    const sControls = document.body.querySelector(`.${sliderControls}`);
+    const sControlList = document.body.querySelectorAll(`.${sliderControl}`);
+    const sItemList = document.body.querySelectorAll(`.${sliderItem}`);
+    let currentSlide = 0;
+
+    sControls.addEventListener("click", function(evt){
+      if ( evt.target.classList.contains(sliderControl) && 
+          !evt.target.classList.contains(sliderControlActive)) {
+            
+            sControlList[currentSlide].classList.remove(sliderControlActive);
+            sItemList[currentSlide].classList.remove(sliderItemActive);
+            
+            evt.target.classList.add(sliderControlActive);
+            currentSlide = Object.keys(sControlList).find(key => sControlList[key] == evt.target);
+            sItemList[currentSlide].classList.add(sliderItemActive);
+      }
+  });
+}
+
+export default createSlider;
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-const temp = () => ({});
+import createSlider from './components/createSlider';
+
+createSlider({  sliderControls: "slider__controls", 
+                sliderControl:  "slider__control",
+                sliderItem:     "slider__item",
+              });
+
+createSlider({  sliderControls: "services__controls", 
+                sliderControl:  "services__control",
+                sliderItem:     "services__item",
+              });
