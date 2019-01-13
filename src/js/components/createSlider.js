b9cbe3f786e6bb0777d3dd2540492dcbb73517c9
SSO <40271618+StanislavSamoilov@users.noreply.github.com>
2019-01-13 14:58:32 +0200

    add popup create function

--- src/js/components/createSlider.js
+++ src/js/components/createSlider.js
@@ -1,26 +1,26 @@
-const createSlider = ({ sliderControls="slider__controls",
-                        sliderControl="slider__control",
+const createSlider = ({ sliderControls='slider__controls',
+                        sliderControl='slider__control',
                         sliderControlActive=`${sliderControl}--active`,
-                        sliderItem="slider__item",
+                        sliderItem='slider__item',
                         sliderItemActive=`${sliderItem}--active`,
                       } = {}) => 
-  {
-    const sControls = document.body.querySelector(`.${sliderControls}`);
-    const sControlList = document.body.querySelectorAll(`.${sliderControl}`);
-    const sItemList = document.body.querySelectorAll(`.${sliderItem}`);
-    let currentSlide = 0;
+{
+  const sControls = document.body.querySelector(`.${sliderControls}`);
+  const sControlList = document.body.querySelectorAll(`.${sliderControl}`);
+  const sItemList = document.body.querySelectorAll(`.${sliderItem}`);
+  let currentSlide = 0;
 
-    sControls.addEventListener("click", function(evt){
-      if ( evt.target.classList.contains(sliderControl) && 
-          !evt.target.classList.contains(sliderControlActive)) {
-            
-            sControlList[currentSlide].classList.remove(sliderControlActive);
-            sItemList[currentSlide].classList.remove(sliderItemActive);
-            
-            evt.target.classList.add(sliderControlActive);
-            currentSlide = Object.keys(sControlList).find(key => sControlList[key] == evt.target);
-            sItemList[currentSlide].classList.add(sliderItemActive);
-      }
+  sControls.addEventListener('click', function(evt){
+    if ( evt.target.classList.contains(sliderControl) && 
+        !evt.target.classList.contains(sliderControlActive)) {
+          
+          sControlList[currentSlide].classList.remove(sliderControlActive);
+          sItemList[currentSlide].classList.remove(sliderItemActive);
+          
+          evt.target.classList.add(sliderControlActive);
+          currentSlide = Object.keys(sControlList).find(key => sControlList[key] == evt.target);
+          sItemList[currentSlide].classList.add(sliderItemActive);
+    }
   });
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,30 @@
 import createSlider from './components/createSlider';
+import createPopUp  from './components/createPopUp';
 
-createSlider({  sliderControls: "slider__controls", 
-                sliderControl:  "slider__control",
-                sliderItem:     "slider__item",
-              });
+createSlider(
+  { 
+    sliderControls: 'slider__controls', 
+    sliderControl:  'slider__control',
+    sliderItem:     'slider__item',
+  }
+);
 
-createSlider({  sliderControls: "services__controls", 
-                sliderControl:  "services__control",
-                sliderItem:     "services__item",
-              });
+createSlider(
+  { 
+    sliderControls: 'services__controls', 
+    sliderControl:  'services__control',
+    sliderItem:     'services__item',
+  }
+);
+
+createPopUp(
+  {
+    popup: 'write-us',
+  }
+);
+
+createPopUp(
+  {
+    popup: 'map',
+  }
+);
